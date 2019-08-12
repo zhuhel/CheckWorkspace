6da4260b3ec55a5f1edf2e180c74bc498ebd01b0
xiangyang.ju <xiangyang.ju@cern.ch>
2019-08-13 01:54:27 +0200

    new setup

--- setup.sh
+++ setup.sh
@@ -6,27 +6,19 @@ currentDir=$PWD
 /bin/grep ' release [2345]\.' /etc/redhat-release >/dev/null 2>&1 && \
   echo "WARNING: This version of the HSG7 ROOT build will not work on an SLC5 machine. Please use a different machine (eg. lxplus.cern.ch)" >&2
 
-# GCC 4.9.3
-PATH="/afs/cern.ch/sw/lcg/contrib/gcc/4.9.3/x86_64-slc6/bin:$PATH"
-LD_LIBRARY_PATH="/afs/cern.ch/sw/lcg/contrib/gcc/4.9.3/x86_64-slc6/lib64:$LD_LIBRARY_PATH"
-
-
-# Python 2.7.4
-PYTHONDIR="/afs/cern.ch/sw/lcg/external/Python/2.7.4/x86_64-slc6-gcc48-opt"
-PATH="$PYTHONDIR/bin:$PATH:/afs/cern.ch/user/x/xju/public/python/bin"
-LD_LIBRARY_PATH="$PYTHONDIR/lib:$LD_LIBRARY_PATH"
-PYTHONPATH=$PYTHONPATH:"$PYTHONDIR/lib/python2.7"
-export PATH LD_LIBRARY_PATH PYTHONDIR PYTHONPATH
+# ROOT and GCC
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source $ATLAS_LOCAL_ROOT_BASE/user/atlasLocalSetup.sh
+lsetup "root 6.08.06-HiggsComb-x86_64-slc6-gcc49-opt"
+# PYTHON
+PYTHONDIR=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/x86_64/python/2.7.13-x86_64-slc6-gcc49/2.7.13-597a5/x86_64-slc6-gcc49-opt
+export PATH=${PYTHONDIR}/bin:$PATH
+# pip
+export PATH=$PATH:/afs/cern.ch/user/x/xju/public/python/bin
+export LD_LIBRARY_PATH="$PYTHONDIR/lib:$LD_LIBRARY_PATH"
 
 # additional lib.
 export PYTHONPATH=$PYTHONPATH:/afs/cern.ch/user/x/xju/public/python/lib/python2.7/site-packages/
 
-#ROOT
-RootDir=/afs/cern.ch/atlas/project/HSG7/root/root_v6-04-02/x86_64-slc6-gcc49/
-cd $RootDir
-source bin/thisroot.sh
-
 # increase stack size - needed for large workspaces
 ulimit -S -s unlimited
-
-cd $currentDir
