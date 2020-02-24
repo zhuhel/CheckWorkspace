730747b531c16b0fe91c1f42ac90c5d479720f56
xiangyang.ju <xiangyang.ju@cern.ch>
2020-02-24 21:36:32 +0100

    up

---
+++ setup.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from setuptools import find_packages
+from setuptools import setup
+
+description="Script to check workspaces"
+
+setup(
+    name="checkws",
+    version="0.1.0",
+    description=description,
+    author="Xiangyang Ju",
+    packages=find_packages(),
+    url='https://gitlab.cern.ch/xju/CheckWorkspace',
+    install_requires=[
+        "argparse",
+        "numpy",
+        'root_plot_utils@ https://github.com/xju2/root_plot_utils/tarball/v1.0.1',
+    ],
+    scripts=[
+        'scripts/check_ws',
+    ]
+)
